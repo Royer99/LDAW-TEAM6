3bd097734a22a7e54dc1931accb148f4d0c18ea0
sayonarachops <A01209929@itesm.mx>
2021-03-16 18:31:02 -0600

    lab1
    falta descripción Royer

--- env-config/routes/web.php
+++ env-config/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get("/", [MateController::class, "listMate"]);
+Route::get("/teammates/{id}", [MateController::class, "specificMate"]); 
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/

9b249565acf60c3eee574e196f7fa003aed65a11
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2021-05-07 16:50:57 -0500

    add: game_seeder

--- proGamers-api/database/seeders/GameSeeder.php
+++ proGamers-api/database/seeders/GameSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class GameSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class GameSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('games')->insert([
+            ['title_id'=>'1','user_id'=>'1','platform_id'=>'1','description'=>'Presenta unos pequnos rayones, pero jala al 100'],
+            ['title_id'=>'2','user_id'=>'2','platform_id'=>'2','description'=>'Presenta unos pequnos rayones, pero jala al 100'],
+            ['title_id'=>'1','user_id'=>'3','platform_id'=>'3','description'=>'Presenta unos pequnos rayones, pero jala al 100'],
+        ]);
     }
 }

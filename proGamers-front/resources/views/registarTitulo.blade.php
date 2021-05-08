2854b6b6211a8b0239ab792c62cbed8ae9c67cf2
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2021-05-07 22:24:42 -0500

    divide view into components

--- proGamers-front/resources/views/registarTitulo.blade.php
+++ proGamers-front/resources/views/registarTitulo.blade.php
@@ -2,37 +2,21 @@
 <meta>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+<link href="{{asset('/css/main.css')}}" rel="stylesheet">
 </meta>
 <body>
     <!--navbar-->
-
+    <x-navbar/>
     <!--sidebar-->
-
+    <div class="container-fluid">
+    <x-sidebar/>
     <!--form-->
-    <form onsubmit="event.preventDefault();" >
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Titulo</label>
-            <input type="text" class="form-control" id="title" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="container-fluid">
+            <x-forms.titulo/>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Descripcion</label>
-            <input type="text" class="form-control" id="description">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Edicion</label>
-            <input type="text" class="form-control" id="edition">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Version</label>
-            <input type="text" class="form-control" id="vesion">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Image</label>
-            <input type="text" class="form-control" id="image">
-        </div>
-        <button id="next" class="btn btn-primary">registrar</button>
-    </form>
+    </main>
+    </div>
     <!--scripts-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>

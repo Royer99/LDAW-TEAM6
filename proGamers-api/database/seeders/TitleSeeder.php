984f50910a93a7394ca95f2834aa006d59db0c93
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2021-06-05 23:37:08 -0500

    fix title seeder

--- proGamers-api/database/seeders/TitleSeeder.php
+++ proGamers-api/database/seeders/TitleSeeder.php
@@ -16,9 +16,9 @@ class TitleSeeder extends Seeder
     public function run()
     {
         DB::table('titles')->insert([
-            ['title'=>'Minecraft','description'=>'Minecraft es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson (conocido comúnmente como "Notch"),2​ y posteriormente desarrollado por su empresa, Mojang Studios. ','edition'=>'Java','version'=>'1.0.6'],
-            ['title'=>'Minecraft','description'=>'Minecraft es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson (conocido comúnmente como "Notch"),2​ y posteriormente desarrollado por su empresa, Mojang Studios. ','edition'=>'Delux','version'=>'2.0.6'],
-            ['title'=>'	Halo: Combat Evolved','description'=>'Halo: Combat Evolved, also known as Halo: CE, is a first-person shooter game developed by Bungie and published by Microsoft Game Studios.','edition'=>'Java','version'=>'1.0.6'],
+            ['title'=>'Minecraft','description'=>'Minecraft es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson (conocido comúnmente como "Notch"),2​ y posteriormente desarrollado por su empresa, Mojang Studios. ','edition'=>'Java','version'=>'1.0.6','image'=>'N/A'],
+            ['title'=>'Minecraft','description'=>'Minecraft es un videojuego de construcción, de tipo «mundo abierto» o sandbox creado originalmente por el sueco Markus Persson (conocido comúnmente como "Notch"),2​ y posteriormente desarrollado por su empresa, Mojang Studios. ','edition'=>'Delux','version'=>'2.0.6','image'=>'N/A'],
+            ['title'=>'	Halo: Combat Evolved','description'=>'Halo: Combat Evolved, also known as Halo: CE, is a first-person shooter game developed by Bungie and published by Microsoft Game Studios.','edition'=>'Java','version'=>'1.0.6','image'=>'N/A'],
         ]);
     }
 }
